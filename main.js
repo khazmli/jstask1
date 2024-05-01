f96ac622ef6243a1903bf424cc09c3616fd5f186
khazmli <togrul.dj@gmail.com>
2024-05-01 09:13:57 +0300

    v1

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("Salam, Xos geldiniz");
+var FirstName = prompt("Adinizi yazin");
+var Surname = prompt("Soyadinizi yazin");
+var YearOfBirth = +prompt("Dogum ilinizi yazin");
+var Result = 2024 - YearOfBirth + "Yas";
+
+alert(`${FirstName} ${Surname} ${Result}`);
+console.log(`${FirstName} ${Surname} ${Result}`);
